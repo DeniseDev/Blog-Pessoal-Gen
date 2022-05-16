68277cf6311427e1891d112da525f502edb3f4f5
DeniseDev <Denise.liiima@outlook.com>
2022-05-16 16:52:38 -0300

    blog pessoal

---
+++ src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
@@ -0,0 +1,135 @@
+package com.blogpessoal.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo nome é obrigatório")	
+	private String nome;
+	
+	@Schema(example = "email@email.com.br")
+
+	
+	@NotNull(message = "O atributo usuário é obrigatório")
+	@Email(message = "O atributo usuário deve ser um e-mail válido!")
+	private String usuario;
+	
+
+	@NotNull(message = "O atributo senha é obrigatório")
+	@Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+	private String senha;
+	
+	
+	@Size(max = 5000, message = "O link da foto não pode conter mais de 50000 caracteres")
+	private String foto;
+	
+	private String tipo;
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto,	String tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.tipo = tipo;
+		
+		
+	}
+
+	
+	public Usuario() {
+		super();
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+	
+	
+}
+
+	
+	
+
